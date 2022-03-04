500b2615178586eb15717cb5cc22ca9db53cbcae
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-03-04 13:48:38 -0300

    Update

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/*Side-bar Section/Seção da barra lateral*/
+let sideBar = document.querySelector('.side-bar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    sideBar.classList.toggle('active');
+}
+
+document.querySelector('#close-side-bar').onclick = () =>{
+    sideBar.classList.remove('active');
+}
